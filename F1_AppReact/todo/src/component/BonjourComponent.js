15f0de569938ad4daca002d83005e88430202d1c
CAMARA <88945535+camara92@users.noreply.github.com>
2023-01-28 06:21:23 +0100

    installation de bootstrap dans le projet react app de todo

---
+++ F1_AppReact/todo/src/component/BonjourComponent.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function BonjourComponent() {
+  
+    return (
+      <div> 
+
+        <h1>Bonjour !</h1>
+      </div>
+    )
+  
+}
+
+
+
+
+export default BonjourComponent; 
+
