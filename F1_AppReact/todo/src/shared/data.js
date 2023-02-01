1b5785da861487745fc85fa6708c69aa7f4b545d
CAMARA <88945535+camara92@users.noreply.github.com>
2023-02-01 01:30:11 +0100

    projet meditation

---
+++ F1_AppReact/todo/src/shared/data.js
@@ -0,0 +1,20 @@
+export const VERSETS = [
+	{
+		titre: "Mr la paix",
+		libele: " Paix et salut à toi et à tous les enfants ! soyez en paix avec vous-même"
+	},
+	{
+		titre: "Mma amour",
+		libele: "C'est le comble même de l'existence et de la paix intérieure. Savoir que l'on existe pour quelqu'un ! "
+	},
+	{
+		titre : "Albert Citation",
+		libele : " L'homme qui considère sa propre vie et celle de ses semblables comme dépourvues de sens n'est pas seulement malheureux, mais difficilement apte à la vie."
+	},
+	{
+		titre : "La loi de la nature",
+		libele : " Je vis donc sans graisse, sans viande, sans poisson, mais je me sens plutôt bien ainsi. Il me semble toujours que l'homme n'est pas né pour être carnivore."
+	}
+	
+
+]
